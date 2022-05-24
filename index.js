5af77c168030b0f7538a4602b8109a90f9c14074
acofer <adam_cofer@yahoo.com>
2022-05-24 19:03:52 -0400

    PLIN-3611: implement cloudflare DNS API methods for query, add, delete

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 "use strict";
 
-const AmazonHandler = require("./lib/route53")
+const AmazonHandler = require("./lib/route53");
+const CloudflareHandler = require("./lib/cloudflare");
 
 const getClient = function(opts) {
 	let subclass;
 	switch(opts.platform) {
 		case "aws":
 			subclass = AmazonHandler;
+			break;
+		case "cloudflare":
+			subclass = CloudflareHandler;
+			break;
 	}
 	return new subclass(opts.domain);
 }
